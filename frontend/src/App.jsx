2040adadc91c8ca031442ac97fe5c80450b03bc4
Winston <leonardwinstoncx9@gmail.com>
2025-12-28 18:15:48 +0800

    Popup to prevent users from refreshing during the game

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 // src/App.jsx
 
+import { useEffect } from "react";
 import HomePage from "./pages/HomePage"; // Adjust the path if you saved it elsewhere
 import PlayerProvider from "./context/PlayerProvider";
 import LobbyPage from "./pages/LobbyPage";
@@ -7,6 +8,21 @@ import GameRoomPage from "./pages/GameRoomPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  useEffect(() => {
+    const handleBeforeUnload = (event) => {
+      const message =
+        "Refreshing will lose all game data. Are you sure you want to refresh?";
+      event.preventDefault();
+      event.returnValue = message;
+      return message;
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
+
   return (
     // All routes inside PlayerProvider can access the player's name
     <PlayerProvider>
